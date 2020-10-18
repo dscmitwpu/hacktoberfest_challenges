8989e452beca9377ef910251725789f408872b2d
shantanu <shantanuvidwans123@gmail.com>
2020-10-18 22:21:49 +0530

    Challenge 1 by Shantanu Vidwans

---
+++ Submissions/challenge_1/shantanu_vidwans.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<stdio.h>
+int main()
+{
+    std::cout << "Welcome to Hacktoberfest 2020!\n";
+     int x,n;
+     scanf("%i",&n);
+     printf("W");
+     for(x=1;x<=n;x++)
+     {
+          printf("o",x);
+     }
+     printf("w");
+     return 0;
+}
